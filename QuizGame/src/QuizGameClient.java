78560c799a2ba9aea67fdc7d86e8393140eca7db
NickBoe <NickBoe@users.noreply.github.com>
2018-10-15 15:38:23 +0200

    Update QuizGame/src/QuizGameClient.java

--- QuizGame/src/QuizGameClient.java
+++ QuizGame/src/QuizGameClient.java
@@ -1,15 +1,9 @@
 
 public class QuizGameClient {
-<<<<<<< HEAD
+
 	public static void main(String [] args) {
 		 System.out.println("Welcome to the quiz client");
-=======
 	
-	
-	public static void main(String[] args) {
-		
->>>>>>> f7b57ce0a00e383071d69f6ee182c21b14d1e718
-		
 	}
 
 }
