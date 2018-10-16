d83464336a74caa2b543d9cf730c3c74aa708631
NickBoe <NickBoe@users.noreply.github.com>
2018-10-16 09:52:20 +0200

    Commented out println for randQuestion

--- QuizGame/src/QuizGameClient.java
+++ QuizGame/src/QuizGameClient.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
 public class QuizGameClient {
 
-	public static void main(String [] args) {
-		 System.out.println("Welcome to the quiz client");
 	
+	
+	public static void main(String [] args) {
+		 Scanner input = new Scanner(System.in);
+		 
+		 System.out.println("Welcome to the quiz client");
+		 //System.out.print(randQuestionFromArray);
+		 String choice = input.next();
+
 	}
 
 }
