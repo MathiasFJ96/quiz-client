286ff4927ac08447ef1f28f23a85532224732df9
Benjamin Bække <benjaabakke@hotmail.com>
2018-10-17 10:24:30 +0200

    Update QuizGame/src/QuizGameClient.java

--- QuizGame/src/QuizGameClient.java
+++ QuizGame/src/QuizGameClient.java
@@ -7,20 +7,25 @@ import java.util.Scanner;
 
 public class QuizGameClient {
 
-	
-	
 	public static void main(String [] args) {
 		 Scanner input = new Scanner(System.in);
-		 
-		 System.out.println("Welcome to the quiz client");
+		 	System.out.println("Welcome to the quiz client");
 		 
 	boolean connect = true;
+	
+	System.out.println("Do you want to continue ? y/n lowercase only");
+	String answer = input.next();
+	
 	try {
 		
 		Socket connectToServer = new Socket("192.168.43.119", 8000);
 		DataInputStream fromServer = new DataInputStream(connectToServer.getInputStream());
 		DataOutputStream toServer = new DataOutputStream(connectToServer.getOutputStream());
 		ObjectInputStream objectFromServer = new ObjectInputStream(connectToServer.getInputStream());
+		
+		if(answer.equals("y")) {
+			toServer.writeInt(1);
+		}
 		
 		while (connect) {
 			System.out.print("");
