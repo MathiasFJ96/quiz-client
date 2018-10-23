2bb6813301a6840c7ed543b580347744399cd683
TheThob <36990151+TheThob@users.noreply.github.com>
2018-10-23 11:54:22 +0200

    Objectoutout fix

--- QuizGame/src/QuizGameClient.java
+++ QuizGame/src/QuizGameClient.java
@@ -2,6 +2,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -21,7 +22,7 @@ public class QuizGameClient {
 	try {
 		
 		Socket connectToServer = new Socket("192.168.43.181", 8200);
-		DataOutputStream toServer = new DataOutputStream(connectToServer.getOutputStream());
+		ObjectOutputStream toServer = new ObjectOutputStream(connectToServer.getOutputStream());
 		ObjectInputStream fromServer = new ObjectInputStream(connectToServer.getInputStream());
 		
 		if(answer.equals("y")) {
